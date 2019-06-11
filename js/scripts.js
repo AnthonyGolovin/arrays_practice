79dfc96095256e0b0cb31cd56edb9465402b4d74
Anthony Golovin, Joseph Tubridy and Tamara Trefilova <tamara@>
2019-06-11 09:53:59 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 var num1;
 var num2;
+var incr;
 
 
 $(function(){
   $("form").submit(function(event) {
     event.preventDefault();
-    // if(isNaN(num1)  || isNaN(num2)) {
-    //   alert("Please enter a valid number");
-    // } else {
       num1 = parseInt($("input#inputNum1").val());
       num2 = parseInt($("input#inputNum2").val());
+      incr = num1;
       // alert(num1+" "+num2);
       if(isNaN(num1)  || isNaN(num2)) {
         alert("Please enter a valid number");
@@ -18,7 +17,7 @@ $(function(){
       } else if (num1 <=0 || num2 <=0) {
           alert("Please enter a number that is higher than 0");
       } else {
-          for (var index = num1; num1 <= num2; num1 += 5) {
+          for (var index = num1; num1 <= num2; num1 += incr) {
             $("#output").append("<li>"+num1+"</li>");
 
         // alert(num1);
